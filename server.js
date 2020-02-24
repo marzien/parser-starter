088e5fae327c04da5f252edb345ea6818e5d1e1a
Marius Zienius <marius.zienius@gmail.com>
2020-02-24 01:21:57 +0100

    aditional routes

--- server.js
+++ server.js
@@ -1,31 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 5000;
+const port = process.env.PORT || 5001;
 
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json({ extended: false }));
 
-app.get('/api/parser', (req, res) => {
-    const result = {
-        version: "HTML5",
-        title: "hello world",
-        headingNumber: 10,
-        headingLevel: "H1",
-        pictureNumber: 5,
-        largestPicture: "picture.jpg",
-        linksIntCount: 3,
-        linksExtCount: 1,
-        inaccesibleLink: 1,
-        loadingTime: 120,
-        httpStatus: "200"
-    };
-
-    res.json(result);
-});
-
-app.post('/api/parser', (req, res) => {
-    console.log(req.body);
-    // TODO: why { URL: '' }
-})
+app.use('/api/parser', require('./backend/routes/parser'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
